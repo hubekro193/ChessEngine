707c1b9b33a7a7abeed9378d04161ff10d9bbff4
HubKro <hubsonix@gmail.com>
2023-09-16 16:06:51 +0200

    fix part 2

--- EngineChess.py
+++ EngineChess.py
@@ -351,8 +351,8 @@ class Move():
     ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
                    "5": 3, "6": 2, "7": 1, "8": 0}
     rowsToRanks = {v: k for k, v in ranksToRows.items()}
-    filesToCols = {"a": 7, "b": 6, "c": 5, "d": 4,
-                   "e": 3, "f": 2, "g": 1, "h": 0}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
     colsToFiles = {v: k for k, v in filesToCols.items()}
     def __init__(self, startSq, endSq, board, isEnpassantMove = False ):
         self.startRow = int(startSq[0])
