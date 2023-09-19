5134029c4265feacd9ddbc0eccb8bff93188f2c2
HubKro <hubsonix@gmail.com>
2023-09-20 00:49:35 +0200

    added simple AI

--- EngineChessMain.py
+++ EngineChessMain.py
@@ -1,5 +1,5 @@
 import pygame as p
-import EngineChess
+import EngineChess, ChessAI
 
 
 WIDTH = HEIGHT = 512
@@ -29,12 +29,15 @@ def main():
     sqSelected = ()
     playerClicks = []
     gameOver = False
+    playerOne = False
+    playerTwo = False
     while running:
+        humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                if not gameOver:
+                if not gameOver and humanTurn:
                     location = p.mouse.get_pos()
                     col = location[0]//SQ_SIZE
                     row = location[1]//SQ_SIZE
@@ -69,6 +72,12 @@ def main():
                     playerClicks = []
                     moveMade = False
                     animate = False
+
+        if not gameOver and not humanTurn:
+            AIMove = ChessAI.findRandomMove(validMoves)
+            gs.makeMove(AIMove)
+            moveMade = True
+            animate = True
 
         if moveMade:
             if animate:
